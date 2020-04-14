4ff6a2bc1f1ada6df9627c0c72519020f19269ac
Reynaldo Gil <gilcu2@gmail.com>
2020-04-14 17:16:02 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,22 @@
+name := "akkaHttpActor"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
+
+lazy val akkaVersion = "2.6.4"
+
+libraryDependencies ++= Seq(
+
+  "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion,
+
+  "org.rogach" %% "scallop" % "3.3.1",
+  "ch.qos.logback" % "logback-classic" % "1.2.3",
+
+  "com.typesafe.akka" %% "akka-http" % "10.1.11",
+  "com.typesafe.akka" %% "akka-stream" % akkaVersion,
+
+  "com.typesafe.akka" %% "akka-actor-testkit-typed" % akkaVersion % Test,
+  "org.scalatest" %% "scalatest" % "3.1.0" % "test"
+
+)
---
+++ src/main/scala/com/gilcu2/Main.scala
@@ -0,0 +1,31 @@
+package com.gilcu2
+
+object QuickstartApp {
+  private def startHttpServer(routes: Route, system: ActorSystem[_]): Unit = {
+    // Akka HTTP still needs a classic ActorSystem to start
+    implicit val classicSystem: akka.actor.ActorSystem = system.toClassic
+    import system.executionContext
+
+    val futureBinding = Http().bindAndHandle(routes, "localhost", 8080)
+    futureBinding.onComplete {
+      case Success(binding) =>
+        val address = binding.localAddress
+        system.log.info("Server online at http://{}:{}/", address.getHostString, address.getPort)
+      case Failure(ex) =>
+        system.log.error("Failed to bind HTTP endpoint, terminating system", ex)
+        system.terminate()
+    }
+  }
+  def main(args: Array[String]): Unit = {
+    val rootBehavior = Behaviors.setup[Nothing] { context =>
+      val userRegistryActor = context.spawn(UserRegistry(), "UserRegistryActor")
+      context.watch(userRegistryActor)
+
+      val routes = new UserRoutes(userRegistryActor)(context.system)
+      startHttpServer(routes.userRoutes, context.system)
+
+      Behaviors.empty
+    }
+    val system = ActorSystem[Nothing](rootBehavior, "HelloAkkaHttpServer")
+  }
+}
